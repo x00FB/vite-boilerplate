5863a17d72cee16cf50b7fa418500d7329907964
x00FB <125113030+x00FB@users.noreply.github.com>
2024-09-20 20:01:50 +0400

    order

--- src/App.tsx
+++ src/App.tsx
@@ -18,35 +18,35 @@ function App() {
       text: userText,
       time: new Date().toLocaleTimeString()
     }
-    setMessages(prevMessages => [...prevMessages, newMessage]);
+    setMessages(prevMessages => [newMessage, ...prevMessages]);
     setUserText('');
   }
 
   return (
     <>
-    <div style={{textAlign: 'left'}}>
-      {/* Display initData */}
-      <div className="card">
-        <p>Init Data: {initData}</p>
+      <div style={{textAlign: 'left'}}>
+        {/* Display initData */}
+        <div className="card">
+          <p>Init Data: {initData}</p>
+        </div>
+        {/* Display messages */}
+        <div className="card">
+          {messages.map((message, index) => (
+            <p key={index}>{message.time}: {message.text}</p>
+          ))}
+        </div>
+        {/* Input form */}
+        <div className="card">
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              value={userText} 
+              onChange={(e) => setUserText(e.target.value)} 
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
-      {/* Display messages */}
-      <div className="card">
-        {messages.reverse().map((message, index) => (
-          <p key={index}>{message.time}: {message.text}</p>
-        ))}
-      </div>
-      {/* Input form */}
-      <div className="card">
-        <form onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            value={userText} 
-            onChange={(e) => setUserText(e.target.value)} 
-          />
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    </div>
     </>
   )
 }
