650d42b86fe00f9c33ace85d598cd36f3ed2e7db
x00FB <125113030+x00FB@users.noreply.github.com>
2024-09-20 17:17:27 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import twaLogo from './assets/tapps.png'
-// import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    console.log(WebApp.initData);
+  }, []);
 
   return (
     <>
-      <div>
-{/*         <a href="https://ton.org/dev" target="_blank">
-          <img src={twaLogo} className="logo" alt="TWA logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-{/*       <h1>TWA + Vite + React</h1> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      <div>{WebApp.initData}</div>
       {/*  */}
       <div className="card">
         <button onClick={() => WebApp.showAlert(`Count is ${count}`)}>
