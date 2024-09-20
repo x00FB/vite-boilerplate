5f46bd04170a0ec56809e6155ff6ffa78cfe5428
x00FB <125113030+x00FB@users.noreply.github.com>
2024-09-20 20:12:52 +0400

    TAAAAAB

--- src/App.tsx
+++ src/App.tsx
@@ -24,29 +24,29 @@ function App() {
 
   return (
     <>
-    <div style={{textAlign: 'left'}}>
-      {/* Display initData */}
-      <div className="card" style={{textAlign: 'left'}}>
-        <p>Init Data: {initData}</p>
+      <div style={{textAlign: 'left'}}>
+        {/* Display initData */}
+        <div className="card" style={{textAlign: 'left'}}>
+          <p>Init Data: {initData}</p>
+        </div>
+        {/* Display messages */}
+        <div className="card">
+          {messages.map((message, index) => (
+            <p key={index}>{message.time}: {message.text}</p>
+          ))}
+        </div>
+        {/* Input form */}
+        <div className="card">
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="text" 
+              value={userText} 
+              onChange={(e) => setUserText(e.target.value)} 
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
-      {/* Display messages */}
-      <div className="card">
-        {messages.map((message, index) => (
-          <p key={index}>{message.time}: {message.text}</p>
-        ))}
-      </div>
-      {/* Input form */}
-      <div className="card">
-        <form onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            value={userText} 
-            onChange={(e) => setUserText(e.target.value)} 
-          />
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    </div>
     </>
   )
 }
