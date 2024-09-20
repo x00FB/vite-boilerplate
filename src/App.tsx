022b87c24c7f0d178fea0536e0c44ec87c816fde
x00FB <125113030+x00FB@users.noreply.github.com>
2024-09-20 20:08:39 +0400

    no_tab

--- src/App.tsx
+++ src/App.tsx
@@ -24,29 +24,29 @@ function App() {
 
   return (
     <>
-      <div style={{textAlign: 'left'}}>
-        {/* Display initData */}
-        <div className="card">
-          <p>Init Data: {initData}</p>
-        </div>
-        {/* Display messages */}
-        <div className="card">
-          {messages.reverse().map((message, index) => (
-            <p key={index}>{message.time}: {message.text}</p>
-          ))}
-        </div>
-        {/* Input form */}
-        <div className="card">
-          <form onSubmit={handleSubmit}>
-            <input 
-              type="text" 
-              value={userText} 
-              onChange={(e) => setUserText(e.target.value)} 
-            />
-            <button type="submit">Send</button>
-          </form>
-        </div>
+    <div style={{textAlign: 'left'}}>
+      {/* Display initData */}
+      <div className="card">
+        <p>Init Data: {initData}</p>
       </div>
+      {/* Display messages */}
+      <div className="card">
+        {messages.reverse().map((message, index) => (
+          <p key={index}>{message.time}: {message.text}</p>
+        ))}
+      </div>
+      {/* Input form */}
+      <div className="card">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={userText} 
+            onChange={(e) => setUserText(e.target.value)} 
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
     </>
   )
 }
